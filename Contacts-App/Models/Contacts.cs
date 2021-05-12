1ff7ef974f9ac4768ff26dbdc1da8b5e78a58b29
Avigail12 <avigailcohen1111@gmail.com>
2021-05-12 12:00:42 +0300

    contracts

---
+++ Contacts-App/Models/Contacts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Contacts_App.Models
+{
+    public partial class Contacts
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Title { get; set; }
+        public byte[] CreatedAt { get; set; }
+        public TimeSpan? UpdatedAt { get; set; }
+    }
+}
---
+++ Contacts-App/Models/contactsappContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Contacts_App.Models
+{
+    public partial class contactsapp : DbContext
+    {
+        public contactsapp()
+        {
+        }
+
+        public contactsapp(DbContextOptions<contactsapp> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Contacts> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=contacts-app;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Contacts>(entity =>
+            {
+                entity.ToTable("contacts");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.CreatedAt)
+                    .HasColumnName("created_at")
+                    .IsRowVersion();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name")
+                    .HasMaxLength(30);
+
+                entity.Property(e => e.Phone)
+                    .IsRequired()
+                    .HasColumnName("phone")
+                    .HasMaxLength(15);
+
+                entity.Property(e => e.Title)
+                    .HasColumnName("title")
+                    .HasMaxLength(10);
+
+                entity.Property(e => e.UpdatedAt).HasColumnName("updated_at");
+            });
+        }
+    }
+}
