3d3eef7dc5536736329b2fd2f741af8a820592c2
Avigail12 <avigailcohen1111@gmail.com>
2021-05-12 14:32:16 +0300

    MESSAGE

--- Contacts-App/Controllers/ContactsController.cs
+++ Contacts-App/Controllers/ContactsController.cs
@@ -24,8 +24,8 @@ namespace Contacts_App.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Contacts>>> GetContacts()
         {
-            var Contacts = await _context.Contacts.ToListAsync();
-            return Ok(Contacts);
+            //var Contacts = await _context.Contacts.ToListAsync();
+            return Ok(await _context.Contacts.ToListAsync());
         }
 
         // GET: api/Contacts/5
@@ -39,7 +39,7 @@ namespace Contacts_App.Controllers
                 return NotFound();
             }
 
-            return contacts;
+            return Ok(contacts);
         }
 
         // PUT: api/Contacts/5
@@ -51,6 +51,7 @@ namespace Contacts_App.Controllers
                 return BadRequest();
             }
 
+            //contacts.UpdatedAt = DateTime.Now// TimeSpan.TryParse(DateTime.Now, out DateTime d);
             _context.Entry(contacts).State = EntityState.Modified;
 
             try
@@ -74,16 +75,37 @@ namespace Contacts_App.Controllers
 
         // POST: api/Contacts
         [HttpPost]
-        public async Task<ActionResult<Contacts>> PostContacts(Contacts contacts)
+        public async Task<ActionResult<Contacts>> PostContacts(Contacts contact)
         {
-            _context.Contacts.Add(contacts);
+            //var Contacts = _context.Contacts.AsQueryable();
+
+            //בד"כ יש אפשרות להגדיר ב מאפייני השדה בטבלה מספור אוטומטי  
+            int Id_Last = _context.Contacts.Max(c => c.Id);
+            contact.Id = Id_Last + 1;
+
+            //בד"כ בדירות ולידציה כאלה עושים מצד הקליינט  
+            if (contact.Phone.Length > 10 || contact.Phone.Length < 9)
+                return BadRequest("מספר טלפון צריך להיות בין 9-10 תווים");
+            if (contact.Phone.Contains("-"))
+                contact.Phone = contact.Phone.Remove(contact.Phone.IndexOf("-"),1);
+            if (!contact.Phone.All(char.IsDigit))
+                return BadRequest("מספר טלפון צריך להיות מספרים בלבד");
+
+            if (contact.Name.Length > 30)
+                return BadRequest("אורך השם מוגבל ל 30 תווים");
+
+            if (contact.Title.Length > 10)
+                return BadRequest("אורך הכותרת מוגבל ל 10 תווים");
+
+            //contact.CreatedAt = DateTime.Now;
+            _context.Contacts.Add(contact);
             try
             {
                 await _context.SaveChangesAsync();
             }
             catch (DbUpdateException)
             {
-                if (ContactsExists(contacts.Id))
+                if (ContactsExists(contact.Id))
                 {
                     return Conflict();
                 }
@@ -93,7 +115,7 @@ namespace Contacts_App.Controllers
                 }
             }
 
-            return CreatedAtAction("GetContacts", new { id = contacts.Id }, contacts);
+            return CreatedAtAction("GetContacts", new { id = contact.Id }, contact);
         }
 
         // DELETE: api/Contacts/5
